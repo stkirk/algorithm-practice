5017f50a3d38a4a7af99c5cc26166b04159b27bb
stkirk <shaanekirk@gmail.com>
2022-05-05 11:51:22 -0400

    setup contains_duplicate problem

---
+++ practice/contains_duplicate.py
@@ -0,0 +1,9 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+def contains_duplicate(nums):
+    return 'foo'
+
+# test cases
+print(contains_duplicate([1,2,3,1])) # True
+print(contains_duplicate([1,2,3,4])) # False
+print(contains_duplicate([1,1,1,3,3,4,3,2,4,2])) # True
