bec49cbd1bb7ef3de01650915621d962858af40a
stkirk <shaanekirk@gmail.com>
2022-05-06 11:20:15 -0400

    setup and test cases

---
+++ practice/word_break.py
@@ -0,0 +1,11 @@
+# Given a string, s, and a list of strings, wordDict:
+# return True if s can be made from the words in wordDict
+# return False if it can't
+# the same word from wordDict can be used multiple times
+def word_break(s, wordDict):
+    return 'foo'
+
+print(word_break("leetcode", ["leet","code"])) # True
+print(word_break("applepenapple",["apple","pen"])) # True
+print(word_break("catsandog",["cats","dog","sand","and","cat"])) # False
+print(word_break("rrcc",["rc"])) # False
