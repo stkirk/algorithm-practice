76e3d1f99338eb7595eba93246116b8145426a5f
stkirk <shaanekirk@gmail.com>
2022-05-06 17:10:53 -0400

    finished with dynamic programming solution

--- practice/word_break.py
+++ practice/word_break.py
@@ -2,8 +2,19 @@
 # return True if s can be made from the words in wordDict
 # return False if it can't
 # the same word from wordDict can be used multiple times
+
 def word_break(s, wordDict):
-    return 'foo'
+    word_model = [False] * (len(s) + 1)
+    word_model[len(s)] = True
+
+    for idx in range(len(s) - 1, -1, -1):
+        for word in wordDict:
+            if (idx + len(word) <= len(s) and s[idx : idx + len(word)] == word):
+                word_model[idx] = word_model[idx + len(word)]
+            if word_model[idx]:
+                break
+
+    return word_model[0]
 
 print(word_break("leetcode", ["leet","code"])) # True
 print(word_break("applepenapple",["apple","pen"])) # True
