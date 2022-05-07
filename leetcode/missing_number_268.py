236d6b7c30346ddcb226df618d4a87b9c0347a71
stkirk <shaanekirk@gmail.com>
2022-05-07 11:45:52 -0400

    renamed folder, set up test cases for missing_number

---
+++ leetcode/missing_number_268.py
@@ -0,0 +1,10 @@
+# Given an array, nums, containing n distinct integers in the range 0 to n
+# return the only number missing from the array
+# ex. if len(nums) is 2, n == 2, the range is 0, 1, 2
+
+def missing_number(nums):
+    return 2
+
+print(missing_number([3,0,1])) # 2
+print(missing_number([0,1])) # 2
+print(missing_number([9,6,4,2,3,5,7,0,1])) # 8
