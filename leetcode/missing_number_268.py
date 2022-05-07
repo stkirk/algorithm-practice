61f494bc97d21a4c450f3d5bbaf654c7063ab179
stkirk <shaanekirk@gmail.com>
2022-05-07 13:24:47 -0400

    solution with plan commented

--- leetcode/missing_number_268.py
+++ leetcode/missing_number_268.py
@@ -3,7 +3,19 @@
 # ex. if len(nums) is 2, n == 2, the range is 0, 1, 2
 
 def missing_number(nums):
-    return 2
+    # sort nums
+    in_order = sorted(nums)
+    # loop over range(len(nums) + 1)
+    for i in range(len(nums) + 1):
+        # safety check to not go out of range of nums
+        if i == (len(nums)):
+            return i
+        # if i, the range index for loop, == nums[i] continues
+        elif i == in_order[i]:
+            continue
+        # else return i because it is missing from nums 
+        return i
+    return -1
 
 print(missing_number([3,0,1])) # 2
 print(missing_number([0,1])) # 2
