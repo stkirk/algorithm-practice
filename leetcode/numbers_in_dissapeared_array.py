d734b7951e9fa7d4e47852069f288c19040df36a
stkirk <shaanekirk@gmail.com>
2022-05-17 12:02:03 -0400

    formed plan for solution in comments

--- leetcode/numbers_in_dissapeared_array.py
+++ leetcode/numbers_in_dissapeared_array.py
@@ -2,7 +2,17 @@
 # Return a list of all the integers in the range [1, n] NOT included in nums
 # Try for solution in O(n) runtime
 
+# Set comprehension--> newSet = { expression for element in iterable } 
+
 def find_disappeared_numbers(nums):
+    # init set for each num in nums, num is key
+    
+    # init return list
+
+    # loop through range 1, n
+        # if num is in the set continue
+        # else add it to return list
+
     return [1]
 
 print(find_disappeared_numbers([4,3,2,7,8,2,3,1])) # [5,6]
