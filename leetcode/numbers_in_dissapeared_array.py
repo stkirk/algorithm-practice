005de33dc1923b20da361ab30219e6ce013ea0a6
stkirk <shaanekirk@gmail.com>
2022-05-17 12:06:24 -0400

    solution passing

--- leetcode/numbers_in_dissapeared_array.py
+++ leetcode/numbers_in_dissapeared_array.py
@@ -6,14 +6,20 @@
 
 def find_disappeared_numbers(nums):
     # init set for each num in nums, num is key
-    
+    num_set = { num for num in nums}
     # init return list
+    disapeared_numbers = []
 
     # loop through range 1, n
+    for num in range(1, len(nums) + 1):
         # if num is in the set continue
+        if num in num_set:
+            continue
         # else add it to return list
+        else:
+            disapeared_numbers.append(num)
 
-    return [1]
+    return disapeared_numbers
 
 print(find_disappeared_numbers([4,3,2,7,8,2,3,1])) # [5,6]
 print(find_disappeared_numbers([1,1])) # [2]
