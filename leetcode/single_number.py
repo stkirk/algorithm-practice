c713b1e017a7606486388736a42bcbde21b9d585
stkirk <shaanekirk@gmail.com>
2022-05-18 11:32:46 -0400

    outlined problem and set up test cases

---
+++ leetcode/single_number.py
@@ -0,0 +1,10 @@
+# Given a non-empty list of ints, nums, every int appears twice except for one. 
+# Return that single non-repeated int
+# Solution must be O(n) Time complexity and O(1) Space complexity
+
+def single_number(nums):
+    return -1
+
+print(single_number([2,2,1])) # 1
+print(single_number([4,1,2,1,2])) # 4
+print(single_number([1])) # 1
