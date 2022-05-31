0bef59db547a293123259586cd788bd260c5696e
stkirk <shaanekirk@gmail.com>
2022-05-31 12:14:54 -0400

    solution passing with memoized dfs

--- leetcode/climbing_stairs.py
+++ leetcode/climbing_stairs.py
@@ -6,14 +6,18 @@
 
 def climb_stairs(n):
     # base case: last two steps each with 1 way to get to nth step
-    
+    first, second = 1, 1
     # loop n-1 times to work back to 0th step
+    for i in range( n - 1):
         # at each step, store old first
+        old_first = first
         # update new first to first plus second
+        first = first + second
         # set second to old first
+        second = old_first
 
     # return first (now at 0th step)    
-    return -1
+    return first
 
 print(climb_stairs(2)) # 2
 print(climb_stairs(3)) # 3
