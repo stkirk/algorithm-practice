9ac386234c8d33f7674694458134adc374e66a75
stkirk <shaanekirk@gmail.com>
2022-05-31 11:21:43 -0400

    setup test cases, problem

---
+++ leetcode/climbing_stairs.py
@@ -0,0 +1,8 @@
+# Given a staircase with n steps, and a restriction that you can only climb 1 or 2 steps at a time
+# In how many distinct ways can you climb to the top?
+
+def climb_stairs(n):
+    return -1
+
+print(climb_stairs(2)) # 2
+print(climb_stairs(3)) # 3
