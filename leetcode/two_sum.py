2af6b9291c621e4a603dcf9c5718aa7c167b4cc3
stkirk <shaanekirk@gmail.com>
2022-06-01 11:12:22 -0400

    setup test cases and problem description

---
+++ leetcode/two_sum.py
@@ -0,0 +1,11 @@
+# Given an array of intergers, nums and an integer, target
+# return a list containing the indicies of the two ints in nums that add up to the target
+# only 1 solution, can't reuse an integer
+# return answer in any order
+
+def two_sum(nums, target):
+    return []
+
+print(two_sum([2,7,11,15], 9)) # [0,1]
+print(two_sum([3,2,4], 6)) # [1,2]
+print(two_sum([3,3], 6)) # [0,1]
