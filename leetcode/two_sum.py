be32ea5b5445fad163eb4234a166ab6a8a8f7880
stkirk <shaanekirk@gmail.com>
2022-06-01 11:42:18 -0400

    solution passing

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -9,11 +9,17 @@ import enum
 def two_sum(nums, target):
     # init dict with complement to num as key, index of num as value
     complements = {(target - num) : i for i, num in enumerate(nums)}
+
     # loop through nums
+    for i, num in enumerate(nums):
         # check if num is in dict
+        if num in complements:
             # if it is, make sure indicies aren't same (no reuse)
+            if i == complements[num]:
+                continue
             # not reused, return index of num and index of complement
-    return complements
+            else:
+                return [i, complements[num]]
 
 print(two_sum([2,7,11,15], 9)) # [0,1]
 print(two_sum([3,2,4], 6)) # [1,2]
