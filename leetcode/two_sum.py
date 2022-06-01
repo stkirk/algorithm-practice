4daac7ae8a3d0395c08b20764c1f7eedea05e423
stkirk <shaanekirk@gmail.com>
2022-06-01 11:37:28 -0400

    dictionary comprehension working for complements

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -3,14 +3,17 @@
 # only 1 solution, can't reuse an integer
 # return answer in any order
 
+import enum
+
+
 def two_sum(nums, target):
     # init dict with complement to num as key, index of num as value
-
+    complements = {(target - num) : i for i, num in enumerate(nums)}
     # loop through nums
         # check if num is in dict
             # if it is, make sure indicies aren't same (no reuse)
             # not reused, return index of num and index of complement
-    return []
+    return complements
 
 print(two_sum([2,7,11,15], 9)) # [0,1]
 print(two_sum([3,2,4], 6)) # [1,2]
