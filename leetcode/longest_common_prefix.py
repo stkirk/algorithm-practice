78f84150e1f0c9da5ef7e399211934bb28bbc26b
stkirk <shaanekirk@gmail.com>
2022-06-03 11:09:42 -0400

    planned solution in comments

--- leetcode/longest_common_prefix.py
+++ leetcode/longest_common_prefix.py
@@ -2,6 +2,17 @@
 # if there is no common prefix, return an empty string
 
 def longest_common_prefix(strs):
+    # init lcp empty string to return
+
+    # can just compare first two strings to get lcp
+    # init str1 = strs[0]
+    # init str2 = strs[1]
+
+    # loop through range min of lengths of str1 and str2
+        # if str1[i] == str2[i]:
+            # lcp += str1[i]
+        # else:
+            # return lcp
     return "strs"
 
 print(longest_common_prefix(["flower","flow","flight"])) # "fl"
