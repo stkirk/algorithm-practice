df36121a1c0816954d42bc823526f4ea939fa879
stkirk <shaanekirk@gmail.com>
2022-06-03 11:00:41 -0400

    setup problem with description and test cases

---
+++ leetcode/longest_common_prefix.py
@@ -0,0 +1,8 @@
+# Given a list of strings, strs, find the longest common prefix and return it
+# if there is no common prefix, return an empty string
+
+def longest_common_prefix(strs):
+    return "strs"
+
+print(longest_common_prefix(["flower","flow","flight"])) # "fl"
+print(longest_common_prefix(["dog","racecar","car"])) # ""
