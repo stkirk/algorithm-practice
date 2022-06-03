971c510ae2399ba4f09bb57506b965640fba48d6
stkirk <shaanekirk@gmail.com>
2022-06-03 11:45:16 -0400

    new plan in comments

--- leetcode/longest_common_prefix.py
+++ leetcode/longest_common_prefix.py
@@ -3,17 +3,19 @@
 
 def longest_common_prefix(strs):
     # init lcp empty string to return
+    lcp = ""
 
-    # can just compare first two strings to get lcp
-    # init str1 = strs[0]
-    # init str2 = strs[1]
-
-    # loop through range min of lengths of str1 and str2
-        # if str1[i] == str2[i]:
-            # lcp += str1[i]
+    # loop through strs
+        # if first string set to lcp
         # else:
-            # return lcp
-    return "strs"
+            # compare new string to lcp letter by letter with loop through min of lengths of the two
+                # init empty string for new_lcp
+                # if lcp[j] != strs[i][j]:
+                    # lcp = new_lcp
+                # else:
+                    # new_lcp += lcp[j]
+    return lcp 
 
 print(longest_common_prefix(["flower","flow","flight"])) # "fl"
+print(longest_common_prefix(["flower","flow","f"])) # "f"
 print(longest_common_prefix(["dog","racecar","car"])) # ""
