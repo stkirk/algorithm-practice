f4f56d0cac6afca985dab3ce4b39ac4d2d5bb3c1
stkirk <shaanekirk@gmail.com>
2022-06-06 12:05:39 -0400

    solution laid out in comments

--- leetcode/maximum_subarray.py
+++ leetcode/maximum_subarray.py
@@ -3,6 +3,20 @@
 # return the sum
 
 def max_subarray(nums):
+    # init a sum to first index value
+
+    # init a current sum to 0 that represents the contiguous subarray sum
+
+    # loop through num in nums
+        # get rid of negative leading index in subarray
+        # if current sum < 0
+            # reset current sum to 0
+
+        # compute new current sum using new num
+        # current sum += num
+        # set max sum to bigger value between exisiting max sum and current sum
+        
+    # return the max sum    
     return -1
 
 print(max_subarray([-2,1,-3,4,-1,2,1,-5,4])) # subarray--> [4,-1,2,1] | sum == 6
