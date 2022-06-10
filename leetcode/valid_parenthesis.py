0032be7ff97ad0dd601ee113df5241a025640026
stkirk <shaanekirk@gmail.com>
2022-06-10 12:50:51 -0400

    wrote solution plan in comments

--- leetcode/valid_parenthesis.py
+++ leetcode/valid_parenthesis.py
@@ -4,6 +4,32 @@
 # open brackets must be closed in the correct order
 
 def is_valid_brackets(s):
+    # init deques for each bracket type
+
+    # loop through bracket in s
+        # if '('
+            # add to paren deque
+        # elif '['
+            # add to square deque
+        # elif '{'
+            # add to curly deque
+        # elif ')'
+            # if len(parens) == 0:
+                # return False
+            # else:
+                # pop a bracket off parens
+        # elif ']'
+            # if len(squares) == 0:
+                # return False
+            # else:
+                # pop a bracket off squares
+        # elif '}'
+            # if len(curlies) == 0:
+                # return False
+            # else:
+                # pop a bracket off curlies
+
+    # if len of all deques is 0            
     return True
 
 print(is_valid_brackets("()")) # True
