7b2bc268d4ffcf3c1bf012a1c54641cb09d46eb6
stkirk <shaanekirk@gmail.com>
2022-06-10 16:59:36 -0400

    wrote second optimized solution leveraging hashmap

--- leetcode/valid_parenthesis.py
+++ leetcode/valid_parenthesis.py
@@ -51,8 +51,42 @@ def is_valid_brackets(s):
     else:
         return False
 
-print(is_valid_brackets("()")) # True
-print(is_valid_brackets("()[]{}")) # True
-print(is_valid_brackets("(]")) # False
-print(is_valid_brackets("()({)}")) # False
-print(is_valid_brackets(")(")) # False
+# print(is_valid_brackets("()")) # True
+# print(is_valid_brackets("()[]{}")) # True
+# print(is_valid_brackets("(]")) # False
+# print(is_valid_brackets("()({)}")) # False
+# print(is_valid_brackets(")(")) # False
+
+# Optimized solution with hashmap
+def is_valid_parenthesis_opt(s):
+    stack = []
+    map_close_to_open = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    for bracket in s:
+        # if its a close bracket
+        if bracket in map_close_to_open:
+            # if the stack has something in it and the last item maps to the close bracket
+            if stack and stack[-1] == map_close_to_open[bracket]:
+                # remove open bracket from stack
+                stack.pop()
+            else:
+                return False
+        # else its an open bracket        
+        else:
+            # add it to the stack
+            stack.append(bracket)
+
+    # if stack is empty, syntax is valid
+    if not stack:
+        return True
+    else:
+        return False
+
+print(is_valid_parenthesis_opt("()")) # True
+print(is_valid_parenthesis_opt("()[]{}")) # True
+print(is_valid_parenthesis_opt("(]")) # False
+print(is_valid_parenthesis_opt("()({)}")) # False
+print(is_valid_parenthesis_opt(")(")) # False
