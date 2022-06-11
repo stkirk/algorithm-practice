b397b8a7584c442e9b9497b97fd2eda82e7cb0d8
stkirk <shaanekirk@gmail.com>
2022-06-11 11:16:04 -0400

    solution plan in comments

--- leetcode/merge_two_sorted_lists.py
+++ leetcode/merge_two_sorted_lists.py
@@ -2,6 +2,7 @@
 # Merge the two lists into one sorted linked list
 # return the head node of the new sorted list
 # Edge case: either or both lists could be empty
+# Edge case: lists could be different lengths
 
 # Definition for singly-linked list.
 class ListNode:
@@ -18,8 +19,26 @@ def print_list(node):
     print(node_list)
 
 def merge_two_lists(list1, list2):
+    # init pointers for current nodes in l1 and l2
+    # init merged list head to return as None
+    # init pointer for merged list as None
 
-    return list1
+    # while loop is there are curs in l1 or l2
+        # if l1.val <= l2.val:
+            # if merged list head is None:
+                # set merged list head to l1
+            # set merged list cur.next to l1
+            # set merged list cur to l1
+            # move cur1 pointer to cur1.next
+
+        # else curl2.val is less than curl1.val
+            #if merged list head is None:
+                # set merge list head to curl2
+            # set merged list cur.next to l2
+            # set merged list cur to l2
+            # move cur1 to cur1.next
+
+    return new_head
 
 # test case linked list nodes
 l11 = ListNode(1)
