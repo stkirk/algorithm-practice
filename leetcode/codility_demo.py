d6423fa26c183bf0b8eeda523d3ccd7314a8eb93
stkirk <shaanekirk@gmail.com>
2022-06-13 16:30:17 -0400

    setup problem with test cases

---
+++ leetcode/codility_demo.py
@@ -0,0 +1,9 @@
+# given a list A of N ints...
+# return the smallest positive int (>0) that does not occur in A
+
+def solution(A):
+    return A
+
+print(solution([1, 3, 6, 4, 1, 2])) # 5
+print(solution([1, 2, 3])) # 4
+print(solution([-1, -3])) # 1
