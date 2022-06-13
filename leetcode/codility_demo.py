d7a009d914af6dcdab84387a28fe447afb527b6a
stkirk <shaanekirk@gmail.com>
2022-06-13 16:37:47 -0400

    solution written in comments

--- leetcode/codility_demo.py
+++ leetcode/codility_demo.py
@@ -2,7 +2,13 @@
 # return the smallest positive int (>0) that does not occur in A
 
 def solution(A):
-    return A
+    # sort A
+    # init min_positive as 1
+
+    # loop through num in A
+        # if num is > 0 and num == min_positive
+            # min_postive += 1
+    # return min_positive 
 
 print(solution([1, 3, 6, 4, 1, 2])) # 5
 print(solution([1, 2, 3])) # 4
