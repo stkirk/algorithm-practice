80af10e6397889f524d830cfc56737011d6222b0
stkirk <shaanekirk@gmail.com>
2022-06-13 16:41:54 -0400

    coded solution and all tests passing

--- leetcode/codility_demo.py
+++ leetcode/codility_demo.py
@@ -3,12 +3,16 @@
 
 def solution(A):
     # sort A
+    A.sort()
     # init min_positive as 1
-
+    min_positive = 1
     # loop through num in A
-        # if num is > 0 and num == min_positive
-            # min_postive += 1
+    for num in A:
+        if num > 0 and num == min_positive:
+            min_positive += 1
+
     # return min_positive 
+    return min_positive
 
 print(solution([1, 3, 6, 4, 1, 2])) # 5
 print(solution([1, 2, 3])) # 4
