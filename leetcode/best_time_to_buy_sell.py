fa25ffa0611c05fd871dc9c2873d0c234167309f
stkirk <shaanekirk@gmail.com>
2022-06-22 16:06:55 -0400

    setup max profit problem with test cases

---
+++ leetcode/best_time_to_buy_sell.py
@@ -0,0 +1,9 @@
+# Given an array of ints, prices, where prices[i] is the price of a given stock on the ith day:
+# Return the maximum profit you can achieve by choosing a day to buy and sell
+# If no profit can be achieved return 0
+
+def max_profit(prices):
+    return prices
+
+print(max_profit([7,1,5,3,6,4])) # 5 --> buy on day 2 sell on day 5
+print(max_profit([7,6,4,3,1])) # 0
