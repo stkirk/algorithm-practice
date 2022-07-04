8bb2674b3aca6e2cdeacd0732de120483a87239f
stkirk <shaanekirk@gmail.com>
2022-07-04 15:43:28 -0400

    setup fizzbuzz

---
+++ leetcode/fizzbuzz.py
@@ -0,0 +1,12 @@
+# Given a number, n, for each integer i in the range 1 to n(inclusive):
+# if i is...
+# a multiple of both 3 and 5 print "FizzBuzz"
+# a multiple of 3 but not 5 print "Fizz"
+# a multiple of 5 but not 3 print "Buzz"
+# not a multiple of either print i
+
+def fizz_buzz(n):
+    print(n)
+
+fizz_buzz(15)
+# prints 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
