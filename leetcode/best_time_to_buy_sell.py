0c3a4a235fff5dbf46503e3a8600bc207c224022
stkirk <shaanekirk@gmail.com>
2022-07-06 11:29:36 -0400

    possible solution written in comment pseudo code

--- leetcode/best_time_to_buy_sell.py
+++ leetcode/best_time_to_buy_sell.py
@@ -3,6 +3,21 @@
 # If no profit can be achieved return 0
 
 def max_profit(prices):
+    # init max_profit to 0
+
+    # memoize the buy index and init buy index to first price in prices
+
+    # loop through i, price in prices
+        # init cur_profit to price - prices[buy_idx]
+
+        # if the cur_profit > max_profit:
+            # set new max profit
+            # max_profit = cur_profit
+
+        # if the price is less than buy index price
+            # set buy index to current index
+
+    # return the max profit
     return prices
 
 print(max_profit([7,1,5,3,6,4])) # 5 --> buy on day 2 sell on day 5
