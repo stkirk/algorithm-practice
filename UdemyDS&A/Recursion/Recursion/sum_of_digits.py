6614455b8486c7c73e795d4bd71d1722a5eb6a25
stkirk <shaanekirk@gmail.com>
2022-07-30 14:54:19 -0400

    added recursive case to sum of digits and started next problem

--- UdemyDS&A/Recursion/Recursion/sum_of_digits.py
+++ UdemyDS&A/Recursion/Recursion/sum_of_digits.py
@@ -15,7 +15,8 @@ def sum_of_digits(n):
     if n == 0:
         return 0
     # Recursive Case:
-    return int( n % 10) + sum_of_digits( n // 10)
+    else:
+        return int( n % 10) + sum_of_digits( n // 10)
 
 print(sum_of_digits(4))
 print(sum_of_digits(11))
