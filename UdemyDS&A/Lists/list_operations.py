1bb95e560ca7063b70d9ba7b2c9de99128c0e86b
stkirk <shaanekirk@gmail.com>
2022-07-31 17:50:23 -0400

    list operations overview

---
+++ UdemyDS&A/Lists/list_operations.py
@@ -0,0 +1,18 @@
+# List Operations and built-in functions
+
+a = [1,2,3]
+b = [4,5,6]
+
+# Concatenate two lists
+c = a + b 
+print(c)
+
+# Repeat x times
+aaa = a * 3
+print(aaa)
+
+# sum all items in list
+# sum = sum(a)
+# print(sum)
+# average of the values in the list
+print(sum(a)/len(a))
